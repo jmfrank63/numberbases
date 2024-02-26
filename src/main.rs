8ea82cf83d3519e2219fe0885296d7ee2998e2f1
Johannes Frank <jmfrank63@gmail.com>
2024-02-26 13:36:16 +0700

    Conversion WIP

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,22 @@
+#![allow(unused_imports)]
+#![allow(unused_variables)]
+#![allow(dead_code)]
+#![allow(unused_mut)]
 use clap::Parser;
+use convert_base::Convert;
 use mlua::prelude::*;
 use num_bigint::BigInt;
+use num_traits::identities::{One, Zero};
 use num_traits::ToPrimitive;
-use num_traits::identities::{Zero, One};
 use serde::Deserialize;
+use std::collections::HashMap;
 use std::fs;
 use std::io::Result;
 use std::io::{Error, ErrorKind};
 
-const HEX_ALPHABET: [&'static str; 16] = [
-    "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F",
-];
+const HEX_ALPHABET: &str = "0123456789ABCDEF";
+const HEX_VALUES: &str = "0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15";
+
 #[derive(Parser, Debug)]
 struct Opt {
     /// Source base
@@ -31,16 +37,16 @@ struct Opt {
     )]
     target_base: isize,
 
-    /// positionber to convert, this is mandatory
-    positionber: isize,
+    /// number to convert, this is mandatory
+    source_number: String,
 
     /// Source alphabet, mandatory if base is greater than 10
-    #[clap(short = 'a', long = "source_alphabet", conflicts_with = "config_file")]
-    source_alphabet: Option<String>,
+    #[clap(short = 'a', long = "source_alphabet", default_value = HEX_ALPHABET, conflicts_with = "config_file")]
+    source_alphabet: String,
 
     /// Alphabet, read from a File
-    #[clap(short = 'b', long = "target_alphabet", conflicts_with = "config_file")]
-    target_alphabet: Option<String>,
+    #[clap(short = 'b', long = "target_alphabet", default_value = HEX_ALPHABET, conflicts_with = "config_file")]
+    target_alphabet: String,
 
     /// Alphabet, read from a File
     #[clap(short = 'c', long = "config_file", conflicts_with_all = &["source_base", "target_base", "source_alphabet", "target_alphabet"], default_value = "config.json")]
@@ -65,8 +71,8 @@ struct Config {
     target: System,
 }
 
-fn calculate_base(lua: &Lua, base: &str, position: isize) -> Result<BigInt> {
-    let calculate_base: mlua::Function = match lua.load(base).eval() {
+fn calculate_base(lua: &Lua, function: &str, position: isize) -> Result<BigInt> {
+    let calculate_base: mlua::Function = match lua.load(function).eval() {
         Ok(f) => f,
         Err(e) => panic!("Error loading lua function: {}", e),
     };
@@ -96,7 +102,10 @@ impl FromBase for BigInt {
         let mut number = number.clone();
 
         while !number.is_zero() {
-            let digit = (&number % base).to_usize().ok_or(std::io::Error::new(std::io::ErrorKind::Other, "Digit too large"))?;
+            let digit = (&number % base).to_u64().ok_or(std::io::Error::new(
+                std::io::ErrorKind::Other,
+                "Digit too large",
+            ))?;
             result = result + digit * &multiplier;
             multiplier = multiplier * base;
             number = number / base;
@@ -113,7 +122,10 @@ impl ToBase for BigInt {
         let mut number = self.clone();
 
         while !number.is_zero() {
-            let digit = (&number % base).to_usize().ok_or(std::io::Error::new(std::io::ErrorKind::Other, "Digit too large"))?;
+            let digit = (&number % base).to_u64().ok_or(std::io::Error::new(
+                std::io::ErrorKind::Other,
+                "Digit too large",
+            ))?;
             result = result + digit * &multiplier;
             multiplier = multiplier * base;
             number = number / base;
@@ -126,7 +138,74 @@ impl ToBase for BigInt {
 fn main() -> Result<()> {
     let opt = Opt::parse();
     println!("{:?}", opt);
-    println!("{:?}", HEX_ALPHABET);
+
+    let source_alphabet_vector = if opt.source_alphabet.contains(',') {
+        opt.source_alphabet.split(',').map(String::from).collect::<Vec<String>>()
+    } else {
+        opt.source_alphabet.chars().map(String::from).collect::<Vec<String>>()
+    };
+
+    let mut source_alphabet_hash: HashMap<String, u64> = HashMap::new();
+    for symbol in source_alphabet_vector.into_iter().enumerate() {
+        source_alphabet_hash.insert(symbol.1, symbol.0 as u64);
+    }
+
+    let target_alphabet_vector = if opt.target_alphabet.contains(',') {
+        opt.target_alphabet.split(',').map(String::from).collect::<Vec<String>>()
+    } else {
+        opt.target_alphabet.chars().map(String::from).collect::<Vec<String>>()
+    };
+
+    let mut target_alphabet_hash: HashMap<String, isize> = HashMap::new();
+    for symbol in target_alphabet_vector.into_iter().enumerate() {
+        target_alphabet_hash.insert(symbol.1, symbol.0 as isize);
+    }
+
+    let source_base_sign = match opt.source_base {
+        base if base < 0 => num_bigint::Sign::Minus,
+        base if base > 0 => num_bigint::Sign::Plus,
+        _ => return Err(std::io::Error::new(
+            std::io::ErrorKind::Other,
+            "Source base cannot be zero",
+        )),
+    };
+
+    let target_base_sign = match opt.target_base {
+        base if base < 0 => num_bigint::Sign::Minus,
+        base if base > 0 => num_bigint::Sign::Plus,
+        _ => return Err(std::io::Error::new(
+            std::io::ErrorKind::Other,
+            "Target base cannot be zero",
+        )),
+    };
+
+    let source_base = BigInt::from(opt.source_base);
+    let target_base = BigInt::from(opt.target_base);
+
+    let source_number = if opt.source_number.contains(",") {
+        opt.source_number.split(',').map(String::from).collect::<Vec<String>>()
+    } else {
+        opt.source_number.chars().map(String::from).collect::<Vec<String>>()
+    };
+
+    let source_number_sign = match source_number[0].as_str() {
+        "-" => num_bigint::Sign::Minus,
+        _ => num_bigint::Sign::Plus,
+    };
+
+    let source_number = if source_number[0].as_str() == "-" {
+        source_number[1..].to_vec()
+    } else {
+        source_number
+    };
+
+    let convert_number = source_number.iter().map(|x| source_alphabet_hash[x]).collect::<Vec<u64>>();
+    
+
+    let mut convert = Convert::new(opt.source_base.abs() as u64, opt.target_base.abs() as u64);
+    let output = convert.convert::<u64,u64>(&convert_number);
+    
+
 
     let lua = Lua::new();
     let config_file_path = opt.config_file;
@@ -148,6 +227,7 @@ fn main() -> Result<()> {
     // TODO: Use the source and target systems to convert positionbers
 
     println!("{} \n {}", config.source.base, config.target.base);
+    println!("Output: {:?}{:?}", source_number_sign, output);
     Ok(())
 }
 
